a9c7b5646b08155a026319fe6d9bf994c960ad51
Shashank23codes <shashankgupta@gmail.com>
2025-03-17 11:34:26 +0530

    Update configuration for environment variables and add render.yaml for deployment

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'root')
-    MYSQL_DB = 'food_donation_system'
+    MYSQL_DB = os.getenv('MYSQL_DB', 'food_donation_system')
     UPLOAD_FOLDER = 'static/uploads'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size 
     STATIC_FOLDER = 'static'
     CSS_FOLDER = 'static/css'
     JS_FOLDER = 'static/js'
     UPLOADS_FOLDER = 'static/uploads'
-    DATETIME_FORMAT = '%Y-%m-%d %H:%M' 
+    DATETIME_FORMAT = '%Y-%m-%d %H:%M'
